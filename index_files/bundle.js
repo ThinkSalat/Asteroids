b8b35dcccb7ceb61b3e81f8635ea67fb3317a508
Shawn Salat <Shawn.Salat@gmail.com>
2018-05-21 18:11:57 -0700

    finalize projects

---
+++ index_files/bundle.js
@@ -0,0 +1,129 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/asteroids.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./lib/asteroid.js":
+/*!*************************!*\
+  !*** ./lib/asteroid.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function getRandomInt(min, max) {\n    return Math.floor(Math.random() * (max - min + 1)) + min;\n}\n\n\nfunction Asteroid(options) {\n  this.COLOR = '#131328';\n  this.RADIUS = 20;\n  let mooptions = {\n    color: this.COLOR,\n    radius:this.RADIUS,\n    pos: options.pos,\n    vel: Util.randomVec(getRandomInt(6,25))\n  };\n  MovingObject.call(this,mooptions);\n}\n\nUtil.inherits(Asteroid,MovingObject);\n\nmodule.exports = Asteroid;\n\n//# sourceURL=webpack:///./lib/asteroid.js?");
+
+/***/ }),
+
+/***/ "./lib/asteroids.js":
+/*!**************************!*\
+  !*** ./lib/asteroids.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("const Util = __webpack_require__(/*! ./util.js */ \"./lib/util.js\");\nwindow.Util = Util;\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./lib/moving_object.js\");\nwindow.MovingObject = MovingObject;\nconst Asteroid = __webpack_require__(/*! ./asteroid.js */ \"./lib/asteroid.js\");\nwindow.Asteroid = Asteroid;\nconst Game = __webpack_require__(/*! ./game.js */ \"./lib/game.js\");\nwindow.Game = Game;\n//-----------------------------------------\n\nconst canvas = document.getElementById('canvas');\nconst ctx = canvas.getContext(\"2d\");\nconst mo = new MovingObject(\n  { pos: [30, 30], vel: [10, 10], radius: 15, color: \"#00FF00\"}\n);\nmo.draw(ctx);\n\nconst aster = new Asteroid({ pos: [200, 200] });\naster.draw(ctx);\n\nconst game = new Game();\ngame.draw(ctx);\n\n\n//# sourceURL=webpack:///./lib/asteroids.js?");
+
+/***/ }),
+
+/***/ "./lib/game.js":
+/*!*********************!*\
+  !*** ./lib/game.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function Game(options){\n  this.DIM_X = 800;\n  this.DIM_Y = 800;\n  this.NUM_ASTEROIDS = 45;\n  this.asteroids = [];\n  this.addAsteroids();\n  \n  \n}\n\nGame.prototype.randomPosition = function(){\n  return [Math.floor(Math.random() * this.DIM_X),Math.floor(Math.random() * this.DIM_Y)];\n};\n\nGame.prototype.addAsteroids = function(){\n  while(this.asteroids.length < this.NUM_ASTEROIDS){\n    this.asteroids.push(new Asteroid({pos:this.randomPosition()}));\n  }\n};\n\nGame.prototype.draw = function(ctx){\n  ctx.clearRect(0, 0, this.DIM_X, this.DIM_Y);\n  this.asteroids.forEach(asteroid => asteroid.draw(ctx));\n};\n\nGame.prototype.moveObjects = function (ctx) {\n  this.asteroids.forEach(asteroid => asteroid.move());\n  this.draw(ctx);\n};\n\nmodule.exports  = Game;\n\n//# sourceURL=webpack:///./lib/game.js?");
+
+/***/ }),
+
+/***/ "./lib/moving_object.js":
+/*!******************************!*\
+  !*** ./lib/moving_object.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function MovingObject(options) {\n  this.pos = options.pos;\n  this.vel = options.vel;\n  this.radius = options.radius;\n  this.color = options.color;\n  \n}\n\nMovingObject.prototype.draw = function (ctx) {\n\n  ctx.fillStyle = this.color;\n  ctx.beginPath();\n\n  ctx.arc(\n    this.pos[0],\n    this.pos[1],\n    this.radius,\n    0,\n    2 * Math.PI,\n    false\n  );\n\n  ctx.fill();\n};\n\nMovingObject.prototype.move = function () {\n  this.pos[0] += this.vel[0];\n  this.pos[1] += this.vel[1];\n};\n\nmodule.exports = MovingObject;\n\n//# sourceURL=webpack:///./lib/moving_object.js?");
+
+/***/ }),
+
+/***/ "./lib/util.js":
+/*!*********************!*\
+  !*** ./lib/util.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("const Util = {\n  inherits(childClass, parentClass) {\n    childClass.prototype =  Object.create(parentClass.prototype);\n    childClass.prototype.constructor = childClass;\n  },\n  // return random vectors \n  randomVec(length) {\n  const deg = 2 * Math.PI * Math.random();\n  return Util.scale([Math.sin(deg), Math.cos(deg)], length);\n  },\n  // Scale the length of a vector by the given amount.\n  scale(vec, m) {\n    return [vec[0] * m, vec[1] * m];\n  }\n};\n\nmodule.exports = Util;\n\n//# sourceURL=webpack:///./lib/util.js?");
+
+/***/ })
+
+/******/ });
